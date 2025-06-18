b648baddb687199dbbb90de58bf6879bb27cf882
imdinnesh <itsdinnesh@gmail.com>
2025-06-18 17:24:24 +0530

    add: seo settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Form from "@/components/form";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-background text-foreground">
-      <Form/>
+      <Head>
+        <title>Online Encryption & Decryption Tool | AES, RSA, Base64</title>
+        <meta
+          name="description"
+          content="Free online tool to encrypt and decrypt text using AES, RSA, Base64, and more. Secure, fast, and easy-to-use."
+        />
+        <link rel="canonical" href="https://crypto.starbyte.tech/" />
+        <meta name="robots" content="index, follow" />
+      </Head>
+      <Form />
     </main>
   );
 }
